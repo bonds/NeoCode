73fe4eb3c232fcd6232b570a7ebf5df0e9b516db
Scott Bonds <scott@ggr.com>
2026-07-09 22:17:05 -0700

    release: 0.8.1-202607100516-4a81554

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
   outputs = { self, nixpkgs }: let
     system = "aarch64-darwin";
     pkgs = nixpkgs.legacyPackages.${system};
-    version = "0.8.1-202607100454-ffee57b";
+    version = "0.8.1-202607100516-4a81554";
   in {
     packages.${system}.default = pkgs.stdenvNoCC.mkDerivation {
       pname = "neocode";
@@ -16,7 +16,7 @@
 
       src = pkgs.fetchurl {
         url = "https://github.com/bonds/NeoCode/releases/download/v${version}/NeoCode.dmg";
-        hash = "sha256-2aaog2m0X9iEuhw7ppRtGfss+cbDnq7cUSXmBH3eXBI=";
+        hash = "sha256-+tgyxoOPqRxWD0cI5x6+lPh8dmT7+Q8wNvupMDMPOl8=";
       };
 
       sourceRoot = ".";
